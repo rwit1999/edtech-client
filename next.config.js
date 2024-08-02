e3e77a07804de163a22bdcf62f30ba7fae8f3768
rwit1999 <wribobeats@gmail.com>
2024-08-02 20:17:55 +0530

    Your commit message describing the changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+
 module.exports = {
     images: {
         domains: ['res.cloudinary.com','randomuser.me'],
     },
+    experimental:{
+        reactRoot:true,
+        suppressHydrationWarning:true
+    }
 };
